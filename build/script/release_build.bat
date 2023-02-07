41213245e817cc89a5f2b46206db6e7315a530db
優米撒那 <93663341+Yomisana@users.noreply.github.com>
2023-02-07 21:08:21 +0800

    刪除不必要的資源

--- build/script/release_build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@REM title Clear Compiler & Build file...
-color 0C
-@REM echo [INFO] Clean old renderer...
-echo [INFO] Run Electron Builder
-npm run bash-release-nsis
-echo [INFO] Done!
-@REM title Clear Compiler & Build file... Done!
