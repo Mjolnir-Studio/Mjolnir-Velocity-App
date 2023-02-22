b3155d5f215f350cfebd8124960432c625227cb6
yomisana <sammywhy3236@gmail.com>
2023-02-22 13:29:02 +0800

    修改dev端點 update frontend

--- app/main-window.js
+++ app/main-window.js
@@ -2,8 +2,7 @@
 const { app, BrowserWindow } = require("electron");
 require('./ipcMain');
 // Create some window settings
-main = new BrowserWindow
-({
+main = new BrowserWindow({
     // 窗口標題
     // 如果HTML標籤 <title> 在由...加載的HTML文件中定義 loadURL(), 此屬性將被忽略。
     title: productName,
@@ -12,13 +11,16 @@ main = new BrowserWindow
     // 透明人間 0.0(完全透明) ~ 1.0 (完全不透明)
     // opacity: 1.0,
     show: true,
-        // if show false, paintWhenInitiallyHidden need false too
-        // paintWhenInitiallyHidden: true,
+    // if show false, paintWhenInitiallyHidden need false too
+    // paintWhenInitiallyHidden: true,
     // 指定父窗口
     // parent: null,
-    width: 1280, height: 720,
-    minWidth: 1280, minHeigh: 720,
-    maxWidth: 1280, maxHeight: 720,
+    width: 1280,
+    height: 720,
+    minWidth: 1280,
+    minHeigh: 720,
+    maxWidth: 1280,
+    maxHeight: 720,
     // width 和 height 將用作網絡 頁面的大小,這意味著實際窗口
     useContentSize: true,
     // Set Background Color
@@ -29,8 +31,8 @@ main = new BrowserWindow
     // center: true,
     // 窗口是否不想要成為無邊框視窗?
     frame: true,
-        // if frame false, 在Windows上,除非窗口無框,否則無法工作。
-        // transparent: true,
+    // if frame false, 在Windows上,除非窗口無框,否則無法工作。
+    // transparent: true,
     // 窗口是否可調整
     resizable: false,
     // 窗口是否可移動 (MacOS Windows)
@@ -45,9 +47,9 @@ main = new BrowserWindow
     // closable: true,
     // 窗口是否可最小化-標題列的 (MacOS Windows)
     // minimizable: true,
-        // 窗口是否可最大化-標題列的 (MacOS Windows)
-        // minimizable 先 false maximizable 也會是 false
-        // maximizable: false,
+    // 窗口是否可最大化-標題列的 (MacOS Windows)
+    // minimizable 先 false maximizable 也會是 false
+    // maximizable: false,
     // 資訊站模式-建議用在Windows上
     // kiosk: false,
     // 自動隱藏菜單欄,除非 Alt 按下鍵。默認值為 false。
@@ -55,11 +57,11 @@ main = new BrowserWindow
     webPreferences: {
         devTools: !app.isPackaged,
         preload: path.join(__dirname, 'main-preload.js'),
-            // Node 關閉不安全的 API
-            nodeIntegration: false,
-                // 通常　nodeIntegration 值是多少 nodeIntegrationInSubFrames 值就是多少
-                nodeIntegrationInSubFrames: false,
-            nodeIntegrationInWorker: false,
+        // Node 關閉不安全的 API
+        nodeIntegration: false,
+        // 通常　nodeIntegration 值是多少 nodeIntegrationInSubFrames 值就是多少
+        nodeIntegrationInSubFrames: false,
+        nodeIntegrationInWorker: false,
         // Disallow http content(cdn...etc)
         allowRunningInsecureContent: false,
         // 預防 前端 js 可以訪問後端的資料
@@ -69,7 +71,7 @@ main = new BrowserWindow
 
 
 // main.center();
-main.once('ready-to-show', async () => {
+main.once('ready-to-show', async() => {
     console.log(`[INFO]  主視窗準備好顯示`);
     main.show();
     splash.destroy();
@@ -77,12 +79,13 @@ main.once('ready-to-show', async () => {
     // main.webContents.send('test',`test message or value`);
 });
 
-if(!app.isPackaged){
+if (!app.isPackaged) {
     main.webContents.openDevTools();
     // main.loadURL(`http://127.0.0.1:3333/`);
     // main.loadURL(`http://127.0.0.1:5173/`);
-    main.loadURL(`http://127.0.0.1:4000/`);
-}else{
+    // main.loadURL(`http://127.0.0.1:4000/`);
+    main.loadURL(`http://127.0.0.1:5500/app/frontend`);
+} else {
     main.setMenu(null);
     main.loadURL(`https://mjolnir.yomisana.xyz/app/velocity/`);
 }
